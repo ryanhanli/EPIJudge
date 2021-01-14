8693a3e1dd2e9765d89ec06c462183a3ca5b0632
Ryan Li <rl3294@nyu.edu>
2021-01-13 20:05:34 -0500

    Finished primitive_multiply

--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def multiply(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    def add(a,b):
+        while b:
+            carry = a&b
+            a,b = a^b, carry<<1
+        return a
+
+    product = 0
+    while x:
+        if x&1:
+            product = add(product,y)
+        x>>=1
+        y<<=1
+    return product
 
 
 if __name__ == '__main__':
