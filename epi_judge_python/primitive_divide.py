9298d0e5c88ec08d666d707ad148feceefdc269f
Ryan Li <rl3294@nyu.edu>
2021-01-17 22:14:26 -0500

    Finished primitive_divide

--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def divide(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    result, power = 0, 32
+    y_power = y<<power
+    while x>=y:
+        while y_power>x:
+            y_power>>=1
+            power-=1
+        result += 1<<power
+        x -= y_power
+    return result
+
 
 
 if __name__ == '__main__':
